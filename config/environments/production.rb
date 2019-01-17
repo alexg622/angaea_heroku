8cb93a77ae70a090f4afc3f6f4f721a2358c5a5a
alexg622 <alex.gm62288@gmail.com>
2019-01-17 12:11:00 -0800

    Make better

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,6 +73,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
    :address              => "smtp.gmail.com",
    :port                 => 587,
+   :domain               => 'www.angaea.com',
    :user_name            => ENV['gmail_username'],
    :password             => ENV['gmail_password'],
    :authentication       => "plain",
