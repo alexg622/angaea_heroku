fc175c02420a33c9f72689f34526e754d03be511
alexg622 <alex.gm62288@gmail.com>
2019-01-22 22:34:18 -0800

    Add images to assets

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,7 +55,8 @@ class UsersController < ApplicationController
   def create_activate_account
     @user = User.find_by(account_activation_secret: params[:activation_id])
     if @user.update_attributes(account_activated: "true")
-      AngaeaActivationMailer.post_activation_email(@user)
+
+      AngaeaActivationMailer.post_activation_email(@user).deliver
       flash.now[:success] = "Account Activated!"
       redirect_to user_path(@user)
     else
