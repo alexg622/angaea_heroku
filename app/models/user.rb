d8fdd240f1d237eefe627bd6100d01351f5b355e
alexg622 <alex.gm62288@gmail.com>
2019-01-31 12:49:44 -0800

    Make better

--- app/models/user.rb
+++ app/models/user.rb
@@ -49,8 +49,12 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
-  validates :city, :state, :address, :zipcode, presence: true 
+  validates :city, :state, :address, :zipcode, presence: true
 
+  def show_location
+    "#{self.city}, #{self.state}"
+  end
+  
   def user_events
     events = {}
     events_arr = []
