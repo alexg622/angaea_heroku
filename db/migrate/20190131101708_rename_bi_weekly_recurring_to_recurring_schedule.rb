48a9e25ae17526150ba69d7dba69982a971af5ad
alexg622 <alex.gm62288@gmail.com>
2019-02-07 21:38:45 -0800

    Make better

--- db/migrate/20190131101708_rename_bi_weekly_recurring_to_recurring_schedule.rb
+++ db/migrate/20190131101708_rename_bi_weekly_recurring_to_recurring_schedule.rb
@@ -1,5 +1,5 @@
-# class RenameBiWeeklyRecurringToRecurringSchedule < ActiveRecord::Migration[5.2]
-#   def change
-#     rename_column :activities, :bi_weekly_recurring, :recurring_schedule
-#   end
-# end
+class RenameBiWeeklyRecurringToRecurringSchedule < ActiveRecord::Migration[5.2]
+  def change
+    rename_column :activities, :bi_weekly_recurring, :recurring_schedule
+  end
+end
