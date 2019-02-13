9ea4559b4116c74ebabc732f9ad53f4a3fb68af4
alexg622 <alex.gm62288@gmail.com>
2019-02-13 11:50:08 -0800

    Make better

--- app/models/category.rb
+++ app/models/category.rb
@@ -14,34 +14,34 @@ class Category < ApplicationRecord
     return [Category.find_by(category_name: "dance"), Category.find_by(category_name: "music"), Category.find_by(category_name: "comedy"), Category.find_by(category_name: "theatre"), Category.find_by(category_name: "food"), Category.find_by(category_name: "casual")]
   end
 
-  def activities_not_passed_date
-    activities_not_overdate = []
-    if self.activities.length > 0
-      self.activities.each do |activity|
-        if activity.start_date >= DateTime.now
-          activities.not_overdate.push(activity)
-        end
-      end
-    end
-    return activities_not_overdate
-  end
+  # def activities_not_passed_date
+  #   activities_not_overdate = []
+  #   if self.activities.length > 0
+  #     self.activities.each do |activity|
+  #       if activity.start_date >= DateTime.now
+  #         activities.not_overdate.push(activity)
+  #       end
+  #     end
+  #   end
+  #   return activities_not_overdate
+  # end
 
-  def sort_valid_activities
-    the_activities = self.activities_not_passed_date
-    not_sorted = true
-    while not_sorted
-      not_sorted = false
-      for i in 0...the_activities.length-1
-        if(the_activities[i].start_date > the_activities[i+1].start_date)
-          prev = the_activities[i]
-          next = the_activities[i+1]
-          the_activities[i] = next
-          the_activities[i+1] = prev
-          not_sorted = true
-        end
-      end
-    end
-    return the_activities
-  end
-  
+  # def sort_valid_activities
+  #   the_activities = self.activities_not_passed_date
+  #   not_sorted = true
+  #   while not_sorted
+  #     not_sorted = false
+  #     for i in 0...the_activities.length-1
+  #       if(the_activities[i].start_date > the_activities[i+1].start_date)
+  #         prev = the_activities[i]
+  #         next = the_activities[i+1]
+  #         the_activities[i] = next
+  #         the_activities[i+1] = prev
+  #         not_sorted = true
+  #       end
+  #     end
+  #   end
+  #   return the_activities
+  # end
+
 end
