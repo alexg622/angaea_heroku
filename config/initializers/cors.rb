e8147eb09f09c5a688e8a3d129ece190a0327cce
alexg622 <alex.gm62288@gmail.com>
2019-06-01 00:35:57 -0700

    better

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000', 'www.angaea.com', 'https://d3ubz66m6sod7m.cloudfront.net'
+    origins 'http://localhost:3000', 'www.angaea.com', 'https://master.d3nlg5vhjsw41.amplifyapp.com', 'https://d3ubz66m6sod7m.cloudfront.net'
 
     resource '*',
       headers: :any,
